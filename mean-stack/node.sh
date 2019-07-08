325e0e20f9cf4774cc77236871e50d805b44fd28
shadowrunnerkyt <shadowrunnerkyt@users.noreply.github.com>
2019-07-08 17:50:14 -0400

    added repo curl for current version of node 10.x

--- mean-stack/node.sh
+++ mean-stack/node.sh
@@ -7,11 +7,13 @@ fi
 echo "Installing the EPEL repository"
 yum -y install epel-release
 echo "Install Node.js"
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
 yum -y install nodejs
 echo "Deploying the application files"
 cp -r /vagrant/appfiles/* $appdir/
 echo "Availing the prerequisites"
 cd $appdir
+npm install express
 npm install
 echo "Making the server.js file executable"
 chmod +x server.js
