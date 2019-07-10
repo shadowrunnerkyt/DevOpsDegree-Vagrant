848aa4a55f8e33a0ee1c1deef0e9be98ea70ca3b
shadowrunnerkyt <shadowrunnerkyt@users.noreply.github.com>
2019-07-09 20:02:54 -0400

    fixed Vagrantfile to make mongoDB listen to all connections (not safest, but cluster is private NAT), and sorted the nginx.sh

--- mean-stack/nginx.sh
+++ mean-stack/nginx.sh
@@ -1,13 +1,13 @@
 webdir=/usr/share/www/html
+if ! [ -d $webdir ]; then
+    mkdir -p $webdir
+fi
 echo "Installing EPEL"
 yum -y install epel-release
 echo "Installing nginx"
 yum -y install nginx
 echo "Deploying the configuration file"
 cp /vagrant/webfiles/etc/nginx/nginx.conf /etc/nginx
-if ! [ -d $webdir ]; then
-    mkdir -p $webdir
-fi
 echo "Deploy the web files"
 cp -r /vagrant/webfiles/usr/share/www/html/* /usr/share/www/html/
 echo "Start the web server"
